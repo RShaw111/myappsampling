c33bff14eb21d793179fe5c145a817fbcd6f4e37
Riaan Shaw <shawriaan@gmail.com>
2020-11-22 13:20:35 +0200

    added to logging file

--- logging_file.py
+++ logging_file.py
@@ -1,12 +1,23 @@
-import logging
-from time import gmtime, strftime
+import logging, sys
+from time import gmtime, strftime, time
+
+timestart = time()
+
+if sys.argv[1] in ('dev', 'sit', 'prd'):
+    print('Environment available')
+else:
+    print("Environment not available")
+    sys.exit()
+
 
 logging.basicConfig(filename='C:/Users/Explore/Desktop/Logtest/Logging.log',level=logging.INFO)
-
+'''
 try:
     x = 3/1
-    logging.info("     {} Update successful".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+    logging.info("      {} snzMains Update successful in {} environment, command took {}sec".format(strftime("%Y-%m-%d %H:%M:%S.0.2f", gmtime()), sys.argv[1], time() - timestart))
 except Exception as e:
     logging.warning("   {} Error occured {}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime()), e))
-    
-print('Done')
+ '''  
+print(strftime("%Y-%m-%d %H:%M:%S.02f", gmtime()))
+print(time() - timestart, "sec")
+print(sys.argv[1])

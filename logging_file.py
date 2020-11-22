27f767c4cdc6f86c51f58123b21ff5753c4d718e
Riaan Shaw <shawriaan@gmail.com>
2020-11-22 14:42:47 +0200

    vs extention

--- logging_file.py
+++ logging_file.py
@@ -14,10 +14,10 @@ logging.basicConfig(filename='C:/Users/Explore/Desktop/Logtest/Logging.log',leve
 '''
 try:
     x = 3/1
-    logging.info("      {} snzMains Update successful in {} environment, command took {}sec".format(strftime("%Y-%m-%d %H:%M:%S.0.2f", gmtime()), sys.argv[1], time() - timestart))
+    logging.info("      {} snzMains Update successful in {} environment, command took {}sec".format(strftime("%Y-%m-%d %H:%M:%S", gmtime()), sys.argv[1], round(time() - timestart, 5)))
 except Exception as e:
-    logging.warning("   {} Error occured {}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime()), e))
- '''  
-print(strftime("%Y-%m-%d %H:%M:%S.02f", gmtime()))
-print(time() - timestart, "sec")
+    logging.warning("   {} Error {}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime()), e))
+'''
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+print(round(time() - timestart, 5), "sec")
 print(sys.argv[1])

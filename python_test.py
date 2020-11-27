580ac160700d3e1919ad2f21df69cef74f365991
Riaan Shaw <shawriaan@gmail.com>
2020-11-27 17:38:26 +0200

    Bearing_calc

--- python_test.py
+++ python_test.py
@@ -1,9 +1,9 @@
-x = 0
-y = 10
+import math
 
-for i in range(y):
-    print(" "*(y-x) + "*"*(2*x-1))
-    x += 1
+def calculate_bearing(X1, Y1, X2, Y2):
+    bearing = math.atan2((X2-X1),(Y2-Y1))*(180/(22/7))
+    return bearing+360
 
-g = (1,9,2,5,5,7)
-print(set(g))
+print(calculate_bearing(527602,178677.3,527600.8,178676))
+
+#print(calculate_bearing(538570.2,181070.7,538580.7,181041))

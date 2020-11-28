93d849a7c5bd35d211f8ad9bec8fc0b6f90d71c4
Riaan Shaw <shawriaan@gmail.com>
2020-11-28 13:20:12 +0200

    added to bearin calc

--- python_test.py
+++ python_test.py
@@ -2,8 +2,11 @@ import math
 
 def calculate_bearing(X1, Y1, X2, Y2):
     bearing = math.atan2((X2-X1),(Y2-Y1))*(180/(22/7))
-    return bearing+360
+    if bearing < 0:
+        return bearing + 360
+    else:
+        return bearing
 
-print(calculate_bearing(527602,178677.3,527600.8,178676))
+print(calculate_bearing(1,3,1,1))
 
 #print(calculate_bearing(538570.2,181070.7,538580.7,181041))
